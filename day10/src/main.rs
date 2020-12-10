7b700c9224f5200cc10cd2eed98da867bb96a3e5
Michael Smedes <smichaelmedes@gmail.com>
2020-12-10 12:24:43 -0500

    clean up

--- day10/src/main.rs
+++ day10/src/main.rs
@@ -14,7 +14,6 @@ fn adapter_combinations(adapters: &[usize]) -> isize {
     for adapter in adapters {
         for difference in 1..4 {
             let previous = (*adapter as isize) - difference;
-            println!("{}, {}", adapter, previous);
             if previous >= 0 {
                 dp[*adapter] += dp[previous as usize];
             }
@@ -48,48 +47,7 @@ fn adapter_distribution(adapters: &mut Vec<usize>) -> usize {
 
 fn read_file(filename: &str) -> Vec<usize> {
     let contents = fs::read_to_string(filename).unwrap();
-    // let contents = "16
-    // 10
-    // 15
-    // 5
-    // 1
-    // 11
-    // 7
-    // 19
-    // 6
-    // 12
-    // 4";
-    // let contents = "28
-    // 33
-    // 18
-    // 42
-    // 31
-    // 14
-    // 46
-    // 20
-    // 48
-    // 47
-    // 24
-    // 23
-    // 49
-    // 45
-    // 19
-    // 38
-    // 39
-    // 11
-    // 1
-    // 32
-    // 25
-    // 35
-    // 8
-    // 17
-    // 7
-    // 9
-    // 4
-    // 2
-    // 34
-    // 10
-    // 3";
+
     contents
         .trim()
         .lines()
