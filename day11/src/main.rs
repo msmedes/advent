8423b74a18851a5c7d4b6f84f6ebe3418e1deee4
Michael Smedes <smichaelmedes@gmail.com>
2020-12-11 18:51:16 -0500

    ok back to where I was

--- day11/src/main.rs
+++ day11/src/main.rs
@@ -8,96 +8,79 @@ enum Seat {
     Empty,
 }
 
+#[derive(Debug, Clone)]
 struct Room {
-    width: usize,
-    height: usize,
-    seats: Vec<Seat>,
+    seats: Vec<Vec<Seat>>,
+    width: isize,
+    height: isize,
+    dirs: Vec<(isize, isize)>,
 }
 
 impl Room {
-    fn new(width: usize, height: usize, seats: Vec<Seat>) -> Room {
+    fn new(seats: Vec<Vec<Seat>>) -> Room {
         Room {
-            width,
-            height,
+            width: seats[0].len() as isize,
+            height: seats.len() as isize,
             seats,
+            dirs: vec![
+                (1, 0),
+                (1, -1),
+                (0, -1),
+                (-1, -1),
+                (-1, 0),
+                (-1, 1),
+                (0, 1),
+                (1, 1),
+            ],
         }
     }
 
-    fn get_index(&self, row: isize, col: isize) -> isize {
-        row * (self.width as isize) + col
+    fn play(&mut self) -> usize {
+        let mut same = false;
+
+        while !same {
+            let mut next = self.seats.clone();
+
+            for row in 0..self.height {
+                for col in 0..self.width {
+                    let neighbors = self.count_neighbors(row, col);
+                    next[row as usize][col as usize] =
+                        match (self.seats[row as usize][col as usize], neighbors) {
+                            (Seat::Empty, 0) => Seat::Occupied,
+                            (Seat::Occupied, 4..=std::i32::MAX) => Seat::Empty,
+                            (seat, _) => seat,
+                        };
+                }
+            }
+            same = next == self.seats;
+            self.seats = next;
+        }
+
+        self.seats
+            .iter()
+            .flatten()
+            .filter(|seat| **seat == Seat::Occupied)
+            .count()
     }
 
-    fn neighbor_count(&self, row: usize, col: usize) -> usize {
+    fn count_neighbors(&self, row: isize, col: isize) -> i32 {
         let mut count = 0;
-        let deltas: Vec<isize> = vec![-1, 0, 1];
-        for delta_row in deltas.iter().cloned() {
-            for delta_col in deltas.iter().cloned() {
-                if delta_col == 0 && delta_row == 0 {
-                    continue;
-                }
-
-                let neighbor_row = row as isize + delta_row;
-                let neighbor_col = col as isize + delta_col;
-                let idx = self.get_index(neighbor_row, neighbor_col);
-                // dbg!(idx);
-                // don't do this. this is bad.
-                if idx < (neighbor_row * self.width as isize) + self.width as isize
-                    && neighbor_col >= 0
-                    && 0 <= idx
-                    && idx < (self.seats.len() as isize)
-                    && self.seats[idx as usize] == Seat::Occupied
-                {
-                    count += 1;
+        for (r, c) in &self.dirs {
+            let (new_row, new_col) = (row + r, col + c);
+            if 0 <= new_row && new_row < self.height && 0 <= new_col && new_col < self.width {
+                if self.seats[new_row as usize][new_col as usize] == Seat::Occupied {
+                    count += 1
                 }
             }
         }
         count
     }
-
-    fn tick(&mut self) -> bool {
-        let mut next = self.seats.clone();
-
-        for row in 0..self.height {
-            for col in 0..self.width {
-                let idx = self.get_index(row as isize, col as isize);
-                let seat = self.seats[idx as usize];
-                let neighbors = self.neighbor_count(row, col);
-                // dbg!(idx, neighbors);
-
-                let next_seat = match (seat, neighbors) {
-                    (Seat::Empty, 0) => Seat::Occupied,
-                    (Seat::Occupied, 4..=std::usize::MAX) => Seat::Empty, // lol
-                    (seat, _) => seat,
-                };
-
-                next[idx as usize] = next_seat;
-            }
-        }
-        // println!("{:?}\n\n{:?}", next, self.seats);
-        if compare_vecs(&next, &self.seats) {
-            println!("{}", self.count_occupied_seats());
-            return true;
-        }
-        self.seats = next;
-        false
-    }
-
-    fn count_occupied_seats(&self) -> usize {
-        self.seats
-            .iter()
-            .filter(|&seat| *seat == Seat::Occupied)
-            .count()
-    }
-}
-
-fn compare_vecs(a: &[Seat], b: &[Seat]) -> bool {
-    a.iter().zip(b).all(|(a, b)| *a == *b)
 }
 
 impl fmt::Display for Room {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        for line in self.seats.as_slice().chunks(self.width) {
-            for &cell in line {
+        for line in &self.seats {
+            for cell in line {
                 let symbol = match cell {
                     Seat::Empty => "L",
                     Seat::Occupied => "#",
@@ -112,47 +95,43 @@ impl fmt::Display for Room {
 }
 
 fn main() {
-    let (seats, width, height) = read_file("input.txt");
-    let mut room = Room::new(width, height, seats);
+    let seats = read_file("input.txt");
+    let mut room = Room::new(seats);
     part1(&mut room);
 }
 
 fn part1(room: &mut Room) {
-    let mut stable = false;
-
-    while !stable {
-        println!("{}", room);
-        stable = room.tick();
-    }
+    let result = room.play();
+    println!("{}", result);
 }
 
-fn read_file(filename: &str) -> (Vec<Seat>, usize, usize) {
-    let contents = fs::read_to_string(filename).unwrap();
-    //     let contents = "L.LL.LL.LL
-    // LLLLLLL.LL
-    // L.L.L..L..
-    // LLLL.LL.LL
-    // L.LL.LL.LL
-    // L.LLLLL.LL
-    // ..L.L.....
-    // LLLLLLLLLL
-    // L.LLLLLL.L
-    // L.LLLLL.LL";
-    let width = contents.find('\n').unwrap();
-    let height = contents.len() / width - 1;
-    println!("{} {}", width, height);
-    let contents = contents.replace("\n", "");
-    (
-        contents
-            .chars()
-            .map(|char| match char {
-                '.' => Seat::Floor,
-                '#' => Seat::Occupied,
-                'L' => Seat::Empty,
-                _ => panic!("whoa now, {}", char),
-            })
-            .collect(),
-        width,
-        height,
-    )
+fn read_file(filename: &str) -> Vec<Vec<Seat>> {
+    // let contents = fs::read_to_string(filename).unwrap();
+    let contents = "L.LL.LL.LL
+    LLLLLLL.LL
+    L.L.L..L..
+    LLLL.LL.LL
+    L.LL.LL.LL
+    L.LLLLL.LL
+    ..L.L.....
+    LLLLLLLLLL
+    L.LLLLLL.L
+    L.LLLLL.LL";
+
+    contents
+        .lines()
+        .map(|line| {
+            line.trim()
+                .to_string()
+                .split("")
+                .filter(|char| *char != "")
+                .map(|char| match char {
+                    "." => Seat::Floor,
+                    "#" => Seat::Occupied,
+                    "L" => Seat::Empty,
+                    _ => panic!("the heck"),
+                })
+                .collect()
+        })
+        .collect()
 }
