115a654c60cd0a6be0c4366a016cf43a210f5bfb
Michael Smedes <smichaelmedes@gmail.com>
2020-12-15 10:54:34 -0500

    refactor

--- day15/src/main.rs
+++ day15/src/main.rs
@@ -1,36 +1,38 @@
-use std::collections::HashMap;
+use std::collections::{HashMap, VecDeque};
 
 fn main() {
-    println!("{}", part1(vec![19, 0, 5, 1, 10, 13], 3000000));
+    println!("{}", part1(vec![19, 0, 5, 1, 10, 13], 30000000));
 }
 
 fn part1(numbers: Vec<usize>, iterations: usize) -> usize {
-    // let numbers: Vec<isize> = vec![19, 0, 5, 1, 10, 13];
-    let mut last_spoken: HashMap<usize, Vec<usize>> = numbers
-        .iter()
-        .enumerate()
-        .map(|(i, num)| (*num, vec![i + 1]))
-        .collect();
+    let mut last_spoken = HashMap::<usize, VecDeque<usize>>::new();
+
+    for (i, num) in numbers.iter().enumerate() {
+        last_spoken.insert(*num, VecDeque::from(vec![i + 1]));
+    }
+
     let mut last_num = numbers[numbers.len() - 1];
 
     for i in numbers.len() + 1..iterations + 1 {
-        let spoken_before = match last_spoken.get(&last_num) {
-            Some(vec) => vec.len() > 1,
-            None => false,
+        last_num = match last_spoken.get(&last_num) {
+            Some(vec) => {
+                let mut curr_num = 0;
+                if vec.len() > 1 {
+                    let spoken_idxs = last_spoken.get(&last_num).unwrap();
+                    curr_num =
+                        spoken_idxs[spoken_idxs.len() - 1] - spoken_idxs[spoken_idxs.len() - 2];
+                }
+                curr_num
+            }
+            None => 0,
         };
-        if !spoken_before {
-            last_spoken
-                .entry(0)
-                .and_modify(|vec| vec.push(i))
-                .or_insert_with(|| vec![i]);
-            last_num = 0;
-        } else {
-            let spoken_idxs = last_spoken.get(&last_num).unwrap();
-            last_num = spoken_idxs[spoken_idxs.len() - 1] - spoken_idxs[spoken_idxs.len() - 2];
-            last_spoken
-                .entry(last_num)
-                .and_modify(|vec| vec.push(i))
-                .or_insert_with(|| vec![i]);
+        last_spoken
+            .entry(last_num)
+            .and_modify(|vec| vec.push_back(i))
+            .or_insert_with(|| VecDeque::from(vec![i]));
+        let queue = last_spoken.get_mut(&last_num).unwrap();
+        if queue.len() > 2 {
+            let _ = queue.pop_front();
         }
     }
     last_num
@@ -42,43 +44,43 @@ mod tests {
     #[test]
     fn test1() {
         let numbers: Vec<usize> = vec![0, 3, 6];
-        let result = part1(numbers);
+        let result = part1(numbers, 2020);
         assert_eq!(result, 436);
     }
     #[test]
     fn test2() {
         let numbers: Vec<usize> = vec![1, 3, 2];
-        let result = part1(numbers);
+        let result = part1(numbers, 2020);
         assert_eq!(result, 1);
     }
     #[test]
     fn test3() {
         let numbers: Vec<usize> = vec![2, 1, 3];
-        let result = part1(numbers);
+        let result = part1(numbers, 2020);
         assert_eq!(result, 10);
     }
     #[test]
     fn test4() {
         let numbers: Vec<usize> = vec![1, 2, 3];
-        let result = part1(numbers);
+        let result = part1(numbers, 2020);
         assert_eq!(result, 27);
     }
     #[test]
     fn test5() {
         let numbers: Vec<usize> = vec![2, 3, 1];
-        let result = part1(numbers);
+        let result = part1(numbers, 2020);
         assert_eq!(result, 78);
     }
     #[test]
     fn test6() {
         let numbers: Vec<usize> = vec![3, 2, 1];
-        let result = part1(numbers);
+        let result = part1(numbers, 2020);
         assert_eq!(result, 438);
     }
     #[test]
     fn test7() {
         let numbers: Vec<usize> = vec![3, 1, 2];
-        let result = part1(numbers);
+        let result = part1(numbers, 2020);
         assert_eq!(result, 1836);
     }
 }
