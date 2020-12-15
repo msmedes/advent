d2f1e9de2c6b9640cf46daafeaea7ff65a756780
Michael Smedes <smichaelmedes@gmail.com>
2020-12-15 12:35:09 -0500

    further refactor

--- day15/src/main.rs
+++ day15/src/main.rs
@@ -1,39 +1,26 @@
-use std::collections::{HashMap, VecDeque};
+use std::collections::HashMap;
 
 fn main() {
     println!("{}", part1(vec![19, 0, 5, 1, 10, 13], 30000000));
 }
 
 fn part1(numbers: Vec<usize>, iterations: usize) -> usize {
-    let mut last_spoken = HashMap::<usize, VecDeque<usize>>::new();
+    let mut last_spoken = HashMap::<usize, usize>::new();
 
     for (i, num) in numbers.iter().enumerate() {
-        last_spoken.insert(*num, VecDeque::from(vec![i + 1]));
+        last_spoken.insert(*num, i);
     }
 
     let mut last_num = numbers[numbers.len() - 1];
 
-    for i in numbers.len() + 1..iterations + 1 {
-        last_num = match last_spoken.get(&last_num) {
-            Some(vec) => {
-                let mut curr_num = 0;
-                if vec.len() > 1 {
-                    let spoken_idxs = last_spoken.get(&last_num).unwrap();
-                    curr_num =
-                        spoken_idxs[spoken_idxs.len() - 1] - spoken_idxs[spoken_idxs.len() - 2];
-                }
-                curr_num
-            }
+    for i in numbers.len()..iterations {
+        let curr_num = match last_spoken.get(&last_num) {
+            Some(last) => i - last,
             None => 0,
         };
-        last_spoken
-            .entry(last_num)
-            .and_modify(|vec| vec.push_back(i))
-            .or_insert_with(|| VecDeque::from(vec![i]));
-        let queue = last_spoken.get_mut(&last_num).unwrap();
-        if queue.len() > 2 {
-            let _ = queue.pop_front();
-        }
+
+        last_spoken.insert(last_num, i);
+        last_num = curr_num;
     }
     last_num
 }
