c490b5ce6744d9ce32a6125ad953dfc98ee2dd8c
Mike Smedes <mike@clearing.com>
2021-12-06 17:23:39 -0500

    day 6

---
+++ 2021/day06/src/main.rs
@@ -0,0 +1,35 @@
+fn main() {
+    let lanternfish: Vec<usize> = include_str!("../input.txt")
+        .split(',')
+        .filter(|x| !x.is_empty())
+        .map(str::parse::<usize>)
+        .map(Result::unwrap)
+        .collect();
+
+    let num_lanternfish = part1(lanternfish);
+    dbg!(num_lanternfish);
+}
+
+fn part1(lanternfish: Vec<usize>) -> usize {
+    // let mut map = HashMap::new();
+
+    let mut fish_counts = vec![0; 9];
+
+    for fish in lanternfish {
+        fish_counts[fish] += 1;
+    }
+
+    for _ in 0..256 {
+        fish_counts = fish_counts
+            .iter()
+            .enumerate()
+            .map(|(index, _)| match index {
+                8 => fish_counts[0],
+                6 => fish_counts[7] + fish_counts[0],
+                other => fish_counts[other + 1],
+            })
+            .collect();
+    }
+
+    fish_counts.iter().sum()
+}
