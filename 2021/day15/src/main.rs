c6264608bd99081e9a0210974b3a139f2349358f
Mike Smedes <mike@clearing.com>
2021-12-15 17:28:31 -0500

    day 15

--- 2021/day15/src/main.rs
+++ 2021/day15/src/main.rs
@@ -1,7 +1,9 @@
+use std::cmp;
 use std::cmp::Reverse;
 use std::collections::BinaryHeap;
 use std::fs;
 use std::str::FromStr;
+
 #[derive(Debug)]
 struct Grid {
     grid: Vec<Vec<usize>>,
@@ -10,42 +12,60 @@ struct Grid {
 }
 
 impl Grid {
-    fn get_neighbors(&self, x: usize, y: usize) -> Vec<(usize, usize)> {
+    fn get_neighbors(&self, x: usize, y: usize, factor: usize) -> Vec<(usize, usize)> {
         let mut neighbors = Vec::new();
         if x > 0 {
             neighbors.push((x - 1, y));
         }
-        if x < self.width - 1 {
+        if x < self.width * factor - 1 {
             neighbors.push((x + 1, y));
         }
         if y > 0 {
             neighbors.push((x, y - 1));
         }
-        if y < self.height - 1 {
+        if y < self.height * factor - 1 {
             neighbors.push((x, y + 1));
         }
         neighbors
     }
 
-    fn calc_cheapest_path(&self) -> Option<usize> {
+    fn calc_cost(&self, x: usize, y: usize) -> usize {
+        let grid_x = x % self.width;
+        let grid_y = y % self.height;
+
+        let x_risk: i64 = (x / self.width) as i64;
+        let y_risk: i64 = (y / self.height) as i64;
+
+        let cost = std::cmp::max(
+            (self.grid[grid_y][grid_x] as i64 + x_risk + y_risk) % 10,
+            self.grid[grid_y][grid_x] as i64 + x_risk + y_risk - 9,
+        );
+        cost as usize
+    }
+
+    fn calc_cheapest_path(&self, factor: usize) -> Option<usize> {
         let mut heap = BinaryHeap::new();
 
-        let mut dist: Vec<Vec<usize>> = (0..self.height)
-            .map(|_| (0..self.width).map(|_| usize::max_value()).collect())
+        let mut dist: Vec<Vec<usize>> = (0..self.height * factor)
+            .map(|_| {
+                (0..self.width * factor)
+                    .map(|_| usize::max_value())
+                    .collect()
+            })
             .collect();
 
         dist[0][0] = 0;
         heap.push((Reverse(0), (0, 0)));
 
         while let Some((Reverse(cost), (x, y))) = heap.pop() {
-            if x == self.width - 1 && y == self.height - 1 {
+            if x == self.width * factor - 1 && y == self.height * factor - 1 {
                 return Some(cost);
             }
 
-            for (nx, ny) in self.get_neighbors(x, y) {
-                let new_cost = cost + self.grid[y][x];
+            for (nx, ny) in self.get_neighbors(x, y, factor) {
+                let new_cost = cost + self.calc_cost(nx, ny);
                 if new_cost < dist[ny][nx] {
-                    dist[nx][ny] = new_cost;
+                    dist[ny][nx] = new_cost;
                     heap.push((Reverse(new_cost), (nx, ny)));
                 }
             }
@@ -80,9 +100,9 @@ impl FromStr for Grid {
 }
 
 fn main() {
-    let input = fs::read_to_string("test_input.txt").expect("Failed to read input");
+    let input = fs::read_to_string("input.txt").expect("Failed to read input");
     let grid = Grid::from_str(&input).unwrap();
 
-    let cost = grid.calc_cheapest_path().unwrap();
+    let cost = grid.calc_cheapest_path(5).unwrap();
     dbg!(cost);
 }
